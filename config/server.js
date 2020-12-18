622558b53f1a0545417ea92e68cc156ebe5b8846
MUÑOZ Olinto <RF5842@engie.com>
2020-12-18 10:13:07 -0300

    firstcommit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2678b43e3e79061a28900ad3aac721a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '29f07d9d-0ac0-497a-aa6d-226629aa82d8'
+};
